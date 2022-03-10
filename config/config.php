6c8221c17ee4d2cc885c7ea274c36d3c153faf27
Johnson2903 <johnsonadelere.29@gmail.com>
2022-03-11 04:08:26 +0800

    add heroku database

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname   = "myFood";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname   = "myFood";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 // if ($conn->connect_error) {
@@ -13,5 +13,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // }
 // echo "Connected successfully";
 
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
